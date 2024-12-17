d7c40a0a4c0f1833646c6edeff5efc7ef1d398c1
ciko22021 <ciko22021@g.nihon-u.ac.jp>
2024-12-17 12:09:14 +0900

    add javafile

--- codes/com/craftinginterpreters/lox/Expr.java
+++ codes/com/craftinginterpreters/lox/Expr.java
@@ -1,4 +1,5 @@
-  static class Binary extends Expr {
+package com.craftinginterpreters.lox;
+ static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
@@ -8,4 +9,4 @@
    final Expr left;
     final Token operator;
     final Expr right;
-  }
+}
